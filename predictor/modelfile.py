beb361853026624637e5d8f76dbd5815f7b05ccf
vikram balaji subrahmanyam <vikrambalaji2004@gmail.com>
2025-01-29 00:25:31 +0530

    files modified for betterment

--- predictor/modelfile.py
+++ predictor/modelfile.py
@@ -6,8 +6,7 @@ from sklearn.preprocessing import MinMaxScaler
 def get_trained_model():
     # Load your data
    
-    data = pd.read_csv(r'C:\Users\Admin\OneDrive\Desktop\blood_donor_prediction\predictor\transfusion.csv')
-    print('im running')
+    data = pd.read_csv('transfusion.csv')
 
 
     # Preprocessing
@@ -23,12 +22,4 @@ def get_trained_model():
     # Train the Random Forest model
     rf_classifier = RandomForestClassifier()
     rf_classifier.fit(X_train, y_train)
-    #aw=np.array([2,6,1500,15])
-    #aq=aw.reshape(1,-1)
-#print(aq)
-    #columns = ['Recency (months)', 'Frequency (times)', 'Monetary (c.c. blood)', 'Time (months)']  # Replace with the actual column names used during training
-    #input_df = pd.DataFrame(aq, columns=columns)
-#print(input_df)
-    #asz=rf_classifier.predict(input_df)
-    #print(asz[0])
     return rf_classifier,scaler
